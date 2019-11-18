21cdd9113b34bde46e2807bf9b34abd458ddac28
JasonLev <jasond.levy@gmail.com>
2019-11-17 18:58:47 -0800

    add table and btn to dashboard

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-
-// import ChoiceForm from "./ChoiceForm";
+import { Link } from "react-router-dom";
 
 export default function Welcome() {
   return (
-    // <Router>
         <Link to="/dashboard">Login Here</Link>
-    // </Router>
     );
 }
