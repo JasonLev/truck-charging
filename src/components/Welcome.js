1dced1e2804c082ccf91a927a7f46cafd0ca48c4
JasonLev <jasond.levy@gmail.com>
2019-11-18 01:52:34 -0800

    add images and landing styles

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import ChargeLogoLanding from "../images/ChargeLogic_Landing.png";
+
+const useStyles = makeStyles(theme => ({
+  // root: {
+  //   padding: theme.spacing(3, 2),
+  //   margin: theme.spacing(1),
+  //   height: "90vh",
+  //   overflowY: "scroll"
+  // },
+  // mapContainer: {
+  //   padding: theme.spacing(3, 2),
+  //   margin: theme.spacing(1),
+  //   // height: "55vh",
+  //   overflowY: "scroll"
+  // },
+  button: {
+    margin: theme.spacing(1)
+  }
+}));
 
 export default function Welcome() {
+  const classes = useStyles();
+
   return (
-        <Link to="/dashboard">Login Here</Link>
-    );
+    <div className="landing">
+      {/*<img src={ChargeLogoLanding} alt="charge logo"/>*/}
+      <Button variant="contained" color="primary" className={classes.button}>
+        <Link to="/dashboard" className="landingBtn">
+          Login Here
+        </Link>
+      </Button>
+    </div>
+  );
 }
